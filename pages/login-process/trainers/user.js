85befffbcd143370339f9d98e802e87b788f0e1d
Zeina Harb <joseph.mhanna@in2uitions.com>
2023-01-11 09:24:47 +0200

    changes

--- pages/login-process/trainers/user.js
+++ pages/login-process/trainers/user.js
@@ -7,7 +7,7 @@ export default function User({ user: {image, userId} }) {
 	const route = (id) => router.push({ pathname: "/login-process/trainers-profile", query: { id } });
 	return (
 		<div>
-		<img className="rounded-full h-20 w-20 border-2 border-[#009FE3] object-fill cursor-pointer" onClick={() => route(userId)} src={`${image_url}${image}`}/>	
+		<img className="rounded-full h-20 w-20 border-2 border-[#009FE3] object-cover cursor-pointer" onClick={() => route(userId)} src={`${image_url}${image}`}/>	
 		</div>
 	);
 }
