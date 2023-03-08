e202d2b9931b2a3ea326999237a63a89fa28e575
Zeina Harb <joseph.mhanna@in2uitions.com>
2023-03-08 13:29:34 +0200

    modification

--- pages/components/compBranches.js
+++ pages/components/compBranches.js
@@ -11,7 +11,7 @@ export default function CompBranches({ data = {}, style = 'white' }) {
         <>
 
             <div className=" lg:my-10 md:pb-10 py-12 container mx-auto px-10 pt-20" >
-                {data.title ?<p className='futura-bold lg:text-5xl'>{data.title}</p>:null}
+                {data.title ?<p className='text-white futura-bold lg:text-5xl'>{data.title}</p>:null}
                 <div className={`table-img lg:grid lg:grid-cols-12 gap-10 pt-10`}>
 
                     {data.branches?.map((item, i) => (
@@ -19,7 +19,7 @@ export default function CompBranches({ data = {}, style = 'white' }) {
                         <>
                         
                             <div className="contact_block col-span-4">
-                                <AnimationOnScroll animateIn="animate__zoomIn" duration={0.3} style={{height:"300px"}}>
+                                <AnimationOnScroll animateIn="animate__zoomIn" duration={1} style={{height:"300px"}}>
                                     {item.branches_details_id?.image ?<img src={`${image_url}${item.branches_details_id?.image?.id}`} altv={item.branches_details_id?.title} /> : null}
                                     <div className='flex flex-col branch-desc'>
                                         <h1 className="img_start absolute bottom-12">{item.branches_details_id?.location}</h1>
