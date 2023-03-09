0ac78d76f2f39dc0ae1fdb1b6e30a124a5d935d2
Zeina Harb <joseph.mhanna@in2uitions.com>
2023-03-09 16:42:19 +0200

    changes

--- pages/components/compFullWidth.js
+++ pages/components/compFullWidth.js
@@ -101,7 +101,7 @@ export default function CompFullWidth({ data = {}, style = 'white' }) {
     return (
         <div className="mt-10 mb-20">
             {data.title ? <div className="lg:mt-40 md:mt-40 lg:mb-40 md:mb-40 mt-20 mb-20 flex flex-col justify-center items-center text-center">
-                <p className="futura-bold text-3xl w-3/4 text-white md:text-6xl lg:text-6xl mx-auto">{data.title}</p>
+                <p className="futura-bold text-3xl w-3/4 text-white md:text-6xl lg:text-6xl mx-auto titleSection">{data.title}</p>
             </div> : null}
             <div className="relative">
                 <div className=" w-screen about-image relative" style={{ "backgroundImage": `url("${image_url}${data.image?.id}")` }}>
