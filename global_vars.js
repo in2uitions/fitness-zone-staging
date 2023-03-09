7c9cee6b5519c8774f1d927415475b3ce052fcad
Zeina Harb <joseph.mhanna@in2uitions.com>
2023-03-09 11:31:03 +0200

    update global vars

--- global_vars.js
+++ global_vars.js
@@ -1,3 +1,3 @@
-export const image_url = "https://fzcms.diastora.com/assets/"
-export const URL = "https://fzcms.diastora.com/"
+export const image_url = "https://cms.fitnesszone.me/assets/"
+export const URL = "https://cms.fitnesszone.me/"
 export const URL_FRONT = "http://localhost:3000/"
