62ee09c64465ab983b532f724d05ca4674acedff
Zeina Harb <joseph.mhanna@in2uitions.com>
2024-01-17 15:26:28 +0200

    classes listing

--- src/components/Branch-classes/index.jsx
+++ src/components/Branch-classes/index.jsx
@@ -4,32 +4,51 @@ import Link from "next/link";
 import tooltipEffect from "../../common/tooltipEffect";
 import worksData from "../../data/sections/worksData.json";
 import { image_url } from "../../../global_vars";
+import { useRouter } from "next/router";
 
 const BranchClasses = ({ data = {} }) => {
-    // const [data, setData] = useState([]);
-
+    const router = useRouter();
     useEffect(() => {
         tooltipEffect();
-        // setData(worksData);
     }, []);
+    const getLocationCode = () => {
+        const { pathname } = router;
+        
+        if (pathname.includes("/baabda/") || pathname.includes("/baabda-homepage/")) {
+            return 2;
+        } else if (pathname.includes("/manara/") || pathname.includes("/manara-homepage/")) {
+            return 9;
+        }else if (pathname.includes("/hamra/") || pathname.includes("/hamra-homepage/")) {
+            return 1;
+        }else if (pathname.includes("/dbayeh/") || pathname.includes("/dbayeh-homepage/")) {
+            return 7;
+        }else if (pathname.includes("/achrafieh/") || pathname.includes("/achrafieh-homepage/")) {
+            return 6;
+        }
 
+        return 1;
+    };
+
+    const addButtonQueryParameter = (url, locationCode) => {
+        return `${url}?locationCode=${locationCode}`;
+    };
     return (
         <section className="works pb-70">
             <h2 style={{ display: 'none' }}> &nbsp; </h2>
             <div className="">
-            <div style={{display:"flex", justifyContent:"center", alignItems:"center", gap:"20px", marginBottom:"3rem"}}>
-                    {data.branch_name? <h1 style={{fontWeight:"bold"}}>{data.branch_name}</h1>:null}
-                    {data.title? <h1 style={{fontWeight:"200",fontFamily:"Montserrat Regular"}}>{data.title}</h1>:null}
+                <div style={{ display: "flex", justifyContent: "center", alignItems: "center", gap: "20px", marginBottom: "3rem" }}>
+                    {data.branch_name ? <h1 style={{ fontWeight: "bold" }}>{data.branch_name}</h1> : null}
+                    {data.title ? <h1 style={{ fontWeight: "200", fontFamily: "Montserrat Regular" }}>{data.title}</h1> : null}
                 </div>
                 <div className="row">
-               
+
                     {data.class_features.map((item, index) => (
-                        <div key={item.id} style={{paddingLeft:"5px", paddingRight:"5px"}} className={`col-lg-4 col-md-6 ${index % 2 !== 0 ? 'valign' : ''}`}>
+                        <div key={item.id} style={{ paddingLeft: "5px", paddingRight: "5px" }} className={`col-lg-4 col-md-6 ${index % 2 !== 0 ? 'valign' : ''}`}>
                             <div className="item">
                                 <Link href="#">
                                     <img src={`${image_url}${item.classes_content_id.image?.id}`} alt="" />
                                 </Link>
-                                <div className="image-text"> 
+                                <div className="image-text">
                                     {index % 3 === 0 && <div className="text text-overImage">{item.classes_content_id?.title[0]?.first}<br></br>{item.classes_content_id?.title[0]?.second}</div>}
                                     {index % 3 === 1 && <div className="text text-overImage">{item.classes_content_id?.title[0]?.first}<br></br>{item.classes_content_id?.title[0]?.second}</div>}
                                     {index % 3 === 2 && <div className="text text-overImage">{item.classes_content_id?.title[0]?.first}<br></br>{item.classes_content_id?.title[0]?.second}</div>}
@@ -38,11 +57,14 @@ const BranchClasses = ({ data = {} }) => {
                         </div>
                     ))}
                 </div>
-                {data.button_title?<div style={{display:"flex", justifyContent:"center", alignItems:"center", marginTop:"2rem"}}>
-                    <button style={{background:"#1990DF", border:"0px solid transparent", borderRadius:"5px", color:"white", height:"3rem", padding:"5px 30px 5px 30px"}}>
+                {data.button_title ?   <a
+                    href={addButtonQueryParameter(data.button_url, getLocationCode())}
+                    style={{ display: "flex", cursor: "pointer", justifyContent: "center", alignItems: "center", marginTop: "2rem" }}
+                >
+                    <button style={{ background: "#1990DF", cursor: "pointer", border: "0px solid transparent", borderRadius: "5px", color: "white", height: "3rem", padding: "5px 30px 5px 30px" }}>
                         {data.button_title}
                     </button>
-                </div>:null}
+                </a> : null}
             </div>
         </section>
     );
