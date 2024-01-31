0d46fabd77e7ce960f3eacd69a781070da510e58
Zeina Harb <joseph.mhanna@in2uitions.com>
2024-01-31 10:56:33 +0200

    changes

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -15,11 +15,11 @@ class MyDocument extends Document {
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="keywords"
-            content="HTML5 Template Avo onepage themeforest"
+            content="Fitness Zone"
           />
           <meta
             name="description"
-            content="Avo - Onepage Multi-Purpose HTML5 Template"
+            content="Fitness Zone"
           />
           <meta name="author" content="" />
           <link rel="shortcut icon" href="/favicon.png" />
